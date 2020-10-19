f756c7816c65a4dcdae444b2bf6c8cacafbe8aba
Mellissa benmeziane <melissabenmeziane01@gmail.com>
2020-10-19 10:55:08 +0200

    cleanUP ( MainLayout)

--- src/components/Menu.vue
+++ src/components/Menu.vue
@@ -16,7 +16,12 @@
           </q-item-section>
         </q-item>
 
-        <q-expansion-item v-else :label="item.title" icon="check">
+        <q-expansion-item
+          default-opened
+          v-else
+          :label="item.title"
+          icon="check"
+        >
           <q-list
             v-for="tiroir in item.multi"
             v-bind:key="tiroir.title"
@@ -34,102 +39,6 @@
         </q-expansion-item>
       </q-list>
     </q-expansion-item>
-    <!--  <q-expansion-item
-      v-if="label.length"
-      :label="label"
-      default-opened
-      class="q-pt-lg"
-      :icon="icon"
-    >
-      <q-item v-if="to.length" :to="to">
-        <q-item-section avatar>
-          <q-icon :name="icon" />
-        </q-item-section>
-        <q-item-section>
-          <q-item-label>{{ title }}</q-item-label>
-        </q-item-section>
-      </q-item>
-      <q-expansion-item default-opened v-else :label="title" icon="check">
-        <q-list
-          v-for="tiroir in multi"
-          v-bind:key="tiroir.title"
-          class="q-pl-lg"
-        >
-          <q-item :to="tiroir.to" class="q-pl-lg">
-            <q-item-section avatar>
-              <q-icon :name="tiroir.icon" />
-            </q-item-section>
-            <q-item-section>
-              <q-item-label>{{ tiroir.title }}</q-item-label>
-            </q-item-section>
-          </q-item>
-        </q-list>
-      </q-expansion-item>
-    </q-expansion-item>
-    <q-expansion-item v-else>
-      <q-list class="q-pl-lg">
-        <q-item v-if="to.length" :to="to">
-          <q-item-section avatar>
-            <q-icon :name="icon" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ title }}</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-expansion-item default-opened v-else :label="title" icon="check">
-          <q-list
-            v-for="tiroir in multi"
-            v-bind:key="tiroir.title"
-            class="q-pl-lg"
-          >
-            <q-item :to="tiroir.to" class="q-pl-lg">
-              <q-item-section avatar>
-                <q-icon :name="tiroir.icon" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>{{ tiroir.title }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item>
-      </q-list>
-    </q-expansion-item>-->
-
-    <!-- <q-expansion-item
-      default-opened
-      class="q-pt-lg"
-      :icon="icon"
-      :label="title"
-    >
-      <q-list class="q-pl-lg">
-        <q-item v-if="to.length" :to="to">
-          <q-item-section avatar>
-            <q-icon :name="icon" />
-          </q-item-section>
-          <q-item-section>
-            <q-item-label>{{ title }}</q-item-label>
-          </q-item-section>
-        </q-item>
-        <q-expansion-item default-opened v-else :label="title" icon="check">
-          <q-list
-            v-for="tiroir in multi"
-            v-bind:key="tiroir.title"
-            class="q-pl-lg"
-          >
-            <q-item :to="tiroir.to" class="q-pl-lg">
-              <q-item-section avatar>
-                <q-icon :name="tiroir.icon" />
-              </q-item-section>
-              <q-item-section>
-                <q-item-label>{{ tiroir.title }}</q-item-label>
-              </q-item-section>
-            </q-item>
-          </q-list>
-        </q-expansion-item>
-      </q-list>
-    </q-expansion-item>-->
-
-    <!--config-->
   </div>
 </template>
 
--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -37,52 +37,8 @@
 </template>
 
 <script>
-import ConfigurationComponents from "components/MenuConfiguration.vue";
-import MontagePlaqueComponents from "components/MenuMontagePlaque.vue";
 import MenuComponents from "components/Menu.vue";
 
-const montageDePlaque = [
-  {
-    title: "Materiels",
-    icon: "check",
-    to: "/materiels"
-  },
-  {
-    title: "Plaques & cables",
-    icon: "check",
-    to: "/cables"
-  },
-  {
-    title: "Fixation des câbles et mini PC",
-    icon: "check",
-    to: "/fixationCables"
-  },
-  {
-    title: "Positionnement adhésif",
-    icon: "check",
-    to: "/positionAdhésif"
-  },
-  {
-    title: "Collage alimentation de l'écran",
-    icon: "check",
-    to: "/collage"
-  },
-  {
-    title: "Adhésif de protection",
-    icon: "check",
-    to: "/adhésifDeProtection"
-  },
-  {
-    title: "Vérification cablages du mini PC",
-    icon: "check",
-    to: "/verificationCables"
-  },
-  {
-    title: "Mise en place | Accroche des câbles",
-    icon: "check",
-    to: "/miseEnPlace"
-  }
-];
 const Menu = [
   {
     label: "Montage de plaque1",
@@ -208,90 +164,15 @@ const Menu = [
     ]
   }
 ];
-const configuration = [
-  {
-    title: "Configuration de base",
-    icon: "check",
-    to: "/configBase"
-  },
-  {
-    title: "Contrôle parental",
-    icon: "check",
-    to: "",
-    multi: [
-      {
-        title: "Mot de passe ",
-        icon: "check",
-        to: "/motdepasse"
-      },
-      {
-        title: "Lier le compte kiddoware",
-        icon: "check",
-        to: "/compteKiddoware"
-      },
-      {
-        title: "Autorisation et bloquage",
-        icon: "check",
-        to: "/autorisation"
-      }
-    ]
-  },
-  {
-    title: "Sauvegarde et restauration",
-    icon: "check",
-    to: "/save"
-  },
-  {
-    title: "Paramètres",
-    icon: "check",
-    to: "",
-    multi: [
-      {
-        title: "Contrôle parental",
-        icon: "check",
-        to: "/parametres"
-      },
-      {
-        title: "Paramètre avancés",
-        icon: "check",
-        to: "/parametreAvances"
-      },
-      {
-        title: "Paramètre interface",
-        icon: "check",
-        to: "/parametreInterface"
-      }
-    ]
-  },
-  {
-    title: "Gestion Utilisateur",
-    icon: "check",
-    to: "/gestionutilisateur"
-  },
-  {
-    title: "Extinction",
-    icon: "check",
-    to: "/extinction"
-  },
-  {
-    title: "Verfication configuration",
-    icon: "check",
-    to: "/verification"
-  }
-];
 
 export default {
   name: "MainLayout",
   components: {
-    MontagePlaqueComponents,
-    ConfigurationComponents,
     MenuComponents
   },
   data() {
     return {
       leftDrawerOpen: false,
-      menuJsonMontageDePlaque: montageDePlaque,
-      menuJsonConfiguration: configuration,
       menuJson: Menu
     };
   }
